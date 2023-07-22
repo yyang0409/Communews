562ff3a7791c1e369153394cc49cfa1b5f21a2a0
Patty Yang <peigen70722@gmail.com>
2023-07-22 14:21:46 +0800

    0722 14:20更新

---
+++ back-end/Word2Vec/word2vec.py
@@ -0,0 +1,28 @@
+from gensim.models import Word2Vec
+
+
+def train(corpus):
+    tokens = [sentence.split() for sentence in corpus]
+
+    # 訓練Word2Vec模型
+    model = Word2Vec(tokens,vector_size=100,window=5,min_count=10,sg=0,hs=1,negative=5,workers=1,epochs=5,sample=0.01)
+
+    # 儲存模型
+    model.save('project_model_v1.model')  
+
+
+def word2vec(kw):
+  try:
+    loaded_model = Word2Vec.load('project_model_v1.model')
+    similar_words = loaded_model.wv.most_similar(kw, topn=10)
+    filtered_words = [word for word, score in similar_words if score > 0.5]
+  except KeyError:
+    filtered_words = "None"
+  return filtered_words
+
+
+
+
+
+
+
