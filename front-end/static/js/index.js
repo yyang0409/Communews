2c0beec1980953dba84eacf2cbf25b4925cbe4e1
Patty Yang <peigen70722@gmail.com>
2023-07-22 18:41:13 +0800

    0722 18:41更新

--- front-end/static/js/index.js
+++ front-end/static/js/index.js
@@ -44,28 +44,46 @@ function printRatingResult(result,num=0){
     result.textContent = `${num}/5`;
 }
 
-function listenforLike(){
+function listenforLike() {
     var likes = document.querySelectorAll(".like");
-    console.log(likes)
-
-        likes.forEach(like => {
-            like.addEventListener("click",(event) => {          
-                    event.target.classList.toggle("like-no");
-                    event.target.classList.toggle("like-yes");
-                    if(event.target.classList.contains("like-yes")) {
-                        console.log("Saving Favorite...");
-                    }
-                    else {
-                        console.log("Remove Favorite...");
-                    }
-                    console.log(like.dataset.id)
-                    // 可回傳like.datset.id => 會是收藏的主題名稱 型態為字串
-                })
-                
-            })// end of forEach
-    
-       
-} // end of listenforLike
+    console.log(likes);
+  
+    likes.forEach(like => {
+      like.addEventListener("click", (event) => {
+        event.target.classList.toggle("like-no");
+        event.target.classList.toggle("like-yes");
+        if (event.target.classList.contains("like-yes")) {
+          console.log("Saving Favorite...");
+        } else {
+          console.log("Remove Favorite...");
+        }
+        var likeStatus = event.target.classList.contains("like-yes") ? "Y" : "N";
+        console.log("Like status:", likeStatus);
+        // 獲取data-id的值
+        var dataIdValue = like.dataset.id;
+  
+        // 將data-id的值透過AJAX POST請求提交到Flask的app.py後端
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/hot", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+              // 成功回傳時執行的程式碼
+              console.log("成功回傳到Flask後端！");
+              console.log(xhr.responseText); // 如果Flask回傳了任何東西，會在瀏覽器的開發者工具中顯示
+            } else {
+              // 發生錯誤時執行的程式碼
+              console.log("傳送至Flask後端時發生錯誤！");
+              console.log(xhr.status);
+            }
+          }
+        };
+        xhr.send("data_id=" + encodeURIComponent(dataIdValue)+ "&like_status=" + likeStatus); // 使用表單格式提交資料
+      });
+    });
+}
+  
 
 
 listenforLike();
